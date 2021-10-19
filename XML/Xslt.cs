64d0f1a9e673ec7b2bed146304e62e0f0e8aa4ad
Beela <bla@senia.dk>
2021-10-19 09:36:42 +0200

    Add project files.

---
+++ XML/Xslt.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Xsl;
+
+namespace XML
+{
+    public class Xslt
+    {
+        public static void Transformer(string xmlSource, string xsltSource, string outputFile)
+        {
+            XslCompiledTransform xslt = new(true);
+            xslt.Load(xsltSource);
+            FileStream outPutStream = new(outputFile, FileMode.Create);
+            xslt.Transform(xmlSource, null, outPutStream);
+        }
+
+        public static void Validation(XmlSchema xsd, string xmlSource)
+        {
+            XmlReaderSettings settings = new();
+            settings.Schemas.Add(xsd);
+            settings.ValidationType = ValidationType.Schema;
+
+            XmlReader reader = XmlReader.Create(xmlSource, settings);
+            XmlDocument xmlDocument = new();
+            xmlDocument.Load(reader);
+            ValidationEventHandler eventHandler = new(ValidationEventHandler);
+            xmlDocument.Validate(eventHandler);
+        }
+
+        public static XmlSchema GetSchema(string xsdSource)
+        {
+            XmlTextReader reader = new(xsdSource);
+            return XmlSchema.Read(reader, ValidationEventHandler);
+        }
+
+
+        static void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            switch (e.Severity)
+            {
+                case XmlSeverityType.Error:
+                    throw new Exception(e.Message);
+
+                case XmlSeverityType.Warning:
+                    throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ XsltTransformer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using XML;
+
+namespace XsltTransformer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Transform");
+            Xslt.Transformer("Book.xml", "XSLT.xslt", "output.xml");
+            Console.WriteLine("Validation");
+            Xslt.Validation(Xslt.GetSchema("TechCollage.xsd"), "root.xml");
+        }
+    }
+
+}
